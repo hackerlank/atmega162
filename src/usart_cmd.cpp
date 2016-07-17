41bf2401ed2b78c47d895a550e4d097df6e8ba88
Zhang Riyueming <github@sowicm.com>
2016-07-17 17:25:07 +0800

    usart_cmd

---
+++ src/usart_cmd.cpp
@@ -0,0 +1,49 @@
+
+#include "utils.h"
+#include "lcd.h"
+#include "usart.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Chip chip;
+LCD lcd;
+USART usart;
+
+char tmp[32];
+
+int main()
+{
+	lcd.init();
+	lcd.drawText(0, 0, "Ready");
+	usart.init(0x33, true);
+	usart.setTrigger(';');
+	while (1)
+	{
+		if (usart.triggered())
+		{
+			lcd.clear();
+			lcd.drawText(0, 0, usart.buf);
+			usart.send(usart.buf);
+
+			if (!strncmp(usart.buf, "beep(", 5))
+			{
+				int n;
+				sscanf(usart.buf, "beep(%d)", &n);
+				if (n < 1)
+					n = 1;
+				beep_b(n);
+			}
+			if (!strncmp(usart.buf, "dis(", 4))
+			{
+				sscanf(usart.buf, "dis(%s", tmp);
+				tmp[ strlen(tmp) - 2 ] = 0;
+				lcd.clear();
+				lcd.drawText(0, 0, tmp);
+			}
+
+			usart.clear_buf();
+		}
+		_delay_ms(500);
+	}
+}
