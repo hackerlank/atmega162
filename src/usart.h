ba8a792059b17520e2350c8f60b4944c9242e1b0
Zhang Riyueming <github@sowicm.com>
2016-07-18 09:43:13 +0800

    usart0, usart1

--- src/usart.h
+++ src/usart.h
@@ -18,27 +18,14 @@ public:
 		_loopback = false;
 	}
 
-	void init(int baud, bool inter)
-	{
-		intr_method = inter;
-		UBRR0H = (byte)(baud >> 8);
-		UBRR0L = (byte)baud;
-		UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (inter ? (1 << RXCIE0) : 0);
-		UCSR0C = (1 << URSEL0) | (0 << USBS0) | (3 << UCSZ00);
-		if (inter)
-			sei();
-	}
+	virtual void init(int baud, bool inter);
 
 	void setTrigger(char c)
 	{
 		trigger = c;
 	}
 
-	void sendbyte(byte c)
-	{
-		while (!(UCSR0A & (1 << UDRE0)));
-			UDR0 = c;
-	}
+	virtual void sendbyte(byte c);
 
 	void send(const char *c)
 	{
@@ -49,20 +36,16 @@ public:
 		}
 	}
 
-	void recvbyte()
+	virtual byte recvbyte();
+
+	virtual void onRXCI()
 	{
-		while (!(UCSR0A & (1 << RXC0)));
-		byte read = UDR0;
+		byte read = recvbyte();
 		if (read == trigger)
 			_triggered = true;
 		add2buf(read);
 		if (_loopback)
 			sendbyte(read);
-	}
-
-	virtual void onRXCI()
-	{
-		recvbyte();
 	}
 
 	bool triggered()
@@ -99,7 +82,7 @@ public:
 	char trigger;
 	bool intr_method;
 //	char buf[10240];
-	char buf[128];
+	char buf[64];
 //	int len;
 	char *buf_tail, *buf_end;
 
@@ -108,9 +91,77 @@ protected:
 	bool _loopback;
 };
 
+class USART0 : public USART
+{
+public:
+	virtual void init(int baud, bool inter)
+	{
+		intr_method = inter;
+		UBRR0H = (byte)(baud >> 8);
+		UBRR0L = (byte)baud;
+		UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (inter ? (1 << RXCIE0) : 0);
+		UCSR0C = (1 << URSEL0) | (0 << USBS0) | (3 << UCSZ00);
+		if (inter)
+			sei();
+	}
+
+	virtual void sendbyte(byte c)
+	{
+		while (!(UCSR0A & (1 << UDRE0)));
+			UDR0 = c;
+	}
+
+	virtual byte recvbyte()
+	{
+		while (!(UCSR0A & (1 << RXC0)));
+		return UDR0;
+	}
+};
+
+class USART1 : public USART
+{
+public:
+	virtual void init(int baud, bool inter)
+	{
+		intr_method = inter;
+		UBRR1H = (byte)(baud >> 8);
+		UBRR1L = (byte)baud;
+		UCSR1B = (1 << RXEN1) | (1 << TXEN1) | (inter ? (1 << RXCIE1) : 0);
+		UCSR1C = (1 << URSEL1) | (0 << USBS1) | (3 << UCSZ10);
+		if (inter)
+			sei();
+	}
+
+	virtual void sendbyte(byte c)
+	{
+		while (!(UCSR1A & (1 << UDRE1)));
+			UDR0 = c;
+	}
+
+	virtual byte recvbyte()
+	{
+		while (!(UCSR1A & (1 << RXC1)));
+		return UDR0;
+	}
+};
+
+#if SINGLE_FILE
+USART0 usart0;
+USART1 usart1;
+#else
+extern USART0 usart0;
+extern USART1 usart1;
+#endif
+
 ISR(USART0_RXC_vect)
 {
-	usart.onRXCI();
+	usart0.onRXCI();
 }
+
+ISR(USART1_RXC_vect)
+{
+	usart1.onRXCI();
+}
+
 
 #endif
