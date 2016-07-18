1c0d0a9053d66e6dcd07bff82444b6d22816f684
Zhang Riyueming <github@sowicm.com>
2016-07-18 11:02:25 +0800

    check both usart

--- src/usart_cmd.cpp
+++ src/usart_cmd.cpp
@@ -8,48 +8,54 @@
 
 LCD lcd;
 
-USART &usart = usart0;
-
 char tmp[32];
+
+void checkCmd(USART& usart)
+{
+	if (usart.triggered())
+	{
+		lcd.clear();
+		lcd.drawText(0, 0, usart.buf);
+		usart.send(usart.buf);
+
+		if (!strncmp(usart.buf, "beep(", 5))
+		{
+			int n;
+			sscanf(usart.buf, "beep(%d)", &n);
+			if (n < 1)
+				n = 1;
+			beep_b(n);
+		}
+		if (!strncmp(usart.buf, "dis(", 4))
+		{
+			sscanf(usart.buf, "dis(%s", tmp);
+			tmp[ strlen(tmp) - 2 ] = 0;
+			lcd.clear();
+			lcd.drawText(0, 0, tmp);
+		}
+		if (!strncmp(usart.buf, "loopback(", 9))
+		{
+			int n;
+			sscanf(usart.buf, "loopback(%d)", &n);
+			usart.loopback(n);
+		}
+
+		usart.clear_buf();
+	}
+}
 
 int main()
 {
 	lcd.init();
 	lcd.drawText(0, 0, "Ready");
-	usart.init(0x33, true);
-	usart.setTrigger(';');
+	usart0.init(0x33, true);
+	usart0.setTrigger(';');
+	usart1.init(0x33, true);
+	usart1.setTrigger(';');
 	while (1)
 	{
-		if (usart.triggered())
-		{
-			lcd.clear();
-			lcd.drawText(0, 0, usart.buf);
-			usart.send(usart.buf);
-
-			if (!strncmp(usart.buf, "beep(", 5))
-			{
-				int n;
-				sscanf(usart.buf, "beep(%d)", &n);
-				if (n < 1)
-					n = 1;
-				beep_b(n);
-			}
-			if (!strncmp(usart.buf, "dis(", 4))
-			{
-				sscanf(usart.buf, "dis(%s", tmp);
-				tmp[ strlen(tmp) - 2 ] = 0;
-				lcd.clear();
-				lcd.drawText(0, 0, tmp);
-			}
-			if (!strncmp(usart.buf, "loopback(", 9))
-			{
-				int n;
-				sscanf(usart.buf, "loopback(%d)", &n);
-				usart.loopback(n);
-			}
-
-			usart.clear_buf();
-		}
+		checkCmd(usart0);
+		checkCmd(usart1);
 		_delay_ms(500);
 	}
 }
