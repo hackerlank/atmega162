b8b57fad7b79221315ee9b1f5582c49821342e84
Zhang Riyueming <github@sowicm.com>
2016-07-19 14:50:50 +0800

    8bit timer

--- src/timer.h
+++ src/timer.h
@@ -43,7 +43,43 @@ public:
 	uint16_t tcnt_val;
 };
 
+class Timer8bit
+{
+public:
+	Timer8bit(volatile uint8_t& _tccr, volatile uint8_t& _tcnt, int _toie)
+		: tccr(_tccr)
+		, tcnt(_tcnt)
+		, toie(_toie)
+	{
+		slot = 0;
+		interval = 0xc2f6;
+	}
+
+	void init()
+	{
+		tcnt_val = 0xffff - interval / (1024 * (1./F_CPU));
+		cli();
+		tccr = (1 << CS12) | (1 << CS10);
+		tcnt = tcnt_val;
+		TIMSK = (1 << toie);
+		sei();
+	}
+
+	void reset()
+	{
+		TCNT1 = tcnt_val;
+	}
+
+	volatile uint8_t &tccr;
+	volatile uint8_t &tcnt;
+	int toie;
+	timer_slot slot;
+	double interval;
+	uint8_t tcnt_val;
+};
+
 Timer timer1(TCCR1A, TCCR1B, TCNT1, TOIE1);
+Timer8bit timer0(TCCR0, TCNT0, TOIE0);
 
 ISR(TIMER1_OVF_vect)
 {
@@ -57,6 +93,20 @@ void setTimer1(double interval, timer_slot slot)
 	timer1.interval = interval;
 	timer1.slot = slot;
 	timer1.init();
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	timer0.reset();
+	if (timer0.slot)
+		(*timer0.slot)();
+}
+
+void setTimer0(double interval, timer_slot slot)
+{
+	timer0.interval = interval;
+	timer0.slot = slot;
+	timer0.init();
 }
 
 #endif
